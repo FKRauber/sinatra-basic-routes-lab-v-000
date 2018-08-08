b4b6c0a7f92ab2c71c9211c2e6424ec61fda29da
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-08-08 00:37:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' { "My name is __" }
+
+  get '/hometown' { "My name is __" }
+
+  get '/favorite-song' { "My name is __" }
+  
 end
